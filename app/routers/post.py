36654e9378977f2a30ac8c3c7f641ec2388c6f2a
DaVicki <vicky.jha24dev@gmail.com>
2024-12-24 10:42:46 -0700

    better dir structure

--- app/routers/post.py
+++ app/routers/post.py
@@ -43,7 +43,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(database.get_db
 @router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, db: Session = Depends(database.get_db), current_user: models.User = Depends(oauth2.get_current_user)):
     post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id == models.Post.id,
-                                                                                       isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
+                                                                                      isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
